bc327a7da81b2705f4ac09ed74674cba3d998eca
Jagdish Singh Rawat <jagdish.singhrawat@degruyter.com>
2024-06-16 23:23:21 +0200

    Part3-2: Phonebook details

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const response = 'Phonebook has info for ' + persons.length + ' people <br/> ' + new Date()
+    res.send(response)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

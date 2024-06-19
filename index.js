df2673287da4fc2228d9a00fa7fbbe1e4bdb2850
Jagdish Singh Rawat <jagdish.singhrawat@degruyter.com>
2024-06-19 23:05:19 +0200

    Part3-8: Phonebook backend step 8

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+const bodyToken = morgan.token('body', (req, res) => {
+    if(req.method === 'POST'){
+        return JSON.stringify(req.body)
+    }
+    return '';
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons =  [
     {
